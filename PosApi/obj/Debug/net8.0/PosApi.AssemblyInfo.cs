4701ffede2d6b00a2dd152fa121a37d31cf71e00
nguyenvanminhhuy <huycattuong0202@gmail.com>
2025-12-19 23:35:46 +0700

    update

--- PosApi/obj/Debug/net8.0/PosApi.AssemblyInfo.cs
+++ PosApi/obj/Debug/net8.0/PosApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PosApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00577e73300a7069543b7edb24d1e616c14a865a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PosApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PosApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
